d4d261d8d065169ab4127590096828c429da4bd4
Sergey Petrunin <neyasut@gmail.com>
2016-05-27 02:43:27 +0300

    proper sample file and aws config

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,4 @@
-if Rails.env.development?
-  Aws.config.update({
-    region: 'us-east-1',
-    credentials: Aws::Credentials.new(Rails.application.secrets.aws_key_id, Rails.application.secrets.aws_key),
-  })
-end
+Aws.config.update({
+  region: 'us-east-1',
+  credentials: Aws::Credentials.new(Rails.application.secrets.aws_key_id, Rails.application.secrets.aws_key),
+})
